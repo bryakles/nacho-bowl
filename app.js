2d6d15d6c15ec28641f63791f5bd6857145e23b4
bryakles <bryakles@gmail.com>
2026-08-07 21:46:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1719,6 +1719,44 @@ function guessNachoBuilderLetter(letter, button) {
     return;
   }
 
+  // Play Spanish letter name
+  const letterNames = {
+    a: "a",
+    b: "be",
+    c: "ce",
+    d: "de",
+    e: "e",
+    f: "efe",
+    g: "ge",
+    h: "hache",
+    i: "i",
+    j: "jota",
+    k: "ka",
+    l: "ele",
+    m: "eme",
+    n: "ene",
+    ñ: "eñe",
+    o: "o",
+    p: "pe",
+    q: "cu",
+    r: "erre",
+    s: "ese",
+    t: "te",
+    u: "u",
+    v: "uve",
+    w: "uve doble",
+    x: "equis",
+    y: "i griega",
+    z: "zeta"
+  };
+
+  const utterance = new SpeechSynthesisUtterance(letterNames[guessedLetter]);
+  utterance.lang = "es-ES";
+  speechSynthesis.cancel();
+  speechSynthesis.speak(utterance);
+
+  nachoBuilderGuessedLetters.add(guessedLetter);
+
   nachoBuilderGuessedLetters.add(guessedLetter);
 
    // Correct guess
